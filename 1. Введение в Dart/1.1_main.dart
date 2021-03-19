eb6b4cd49a952b262fd6046e95f83108dd0bf229
Anna-Myzukina <anna.muzykina83@gmail.com>
2021-03-19 13:27:17 +0300

    Add example to first lesson

---
+++ 1. Введение в Dart/1.1_main.dart
@@ -0,0 +1,10 @@
+// Определяем функцию
+void printInteger(int aNumber) {
+ print('The number is $aNumber.'); // Выводим в консоль
+}
+
+// Здесь начинается выполнение приложения 
+void main() {
+ var number = 42; // Объявление и инициализация переменной.
+ printInteger(number); // ​​​​​​​Вызов функции
+}
