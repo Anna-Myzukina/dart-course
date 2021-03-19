8379fe01e58040d5c5f4ffad6e01a5577f402143
Anna-Myzukina <anna.muzykina83@gmail.com>
2021-03-19 18:57:07 +0300

    Add example 5.4

---
+++ 5. Переменные и константы/5.4_main.dart
@@ -0,0 +1,13 @@
+enum Status {
+  none,
+  running,
+  stopped,
+  paused
+}
+
+void main() {
+  print(Status.values);
+  Status.values.forEach((v) => print('value: v, index:v,index:{v.index}'));
+  print('running: {Status.running},Status.running,{Status.running.index}');
+  print('running index: ${Status.values[1]}');
+}
